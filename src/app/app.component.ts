c1cffbc920645073bee642b06c0d05b00f712ea1
damon leong <damon@cloud-ace.com>
2020-09-19 08:51:22 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'daofab';
+  email: any;
+  error: any;
+  submitted = false;
+
+  constructor(private http: HttpClient) { }
+
+  subcribe() {
+    // const params = new HttpParams()
+    //   .set('email', this.email);
+    if (this.email) {
+      const url = 'https://daofab.us17.list-manage.com/subscribe/post-json?u=4895a97c722bbb88e2672fd4e&amp;id=af595c3f11&email='
+        + this.email;
+      this.http.jsonp(url,
+        'c').subscribe((res: any) => {
+          console.log(res.result);
+          if (res.result && res.result !== 'error') {
+            alert('Thank you for subscribing to our mailing list.');
+          } else {
+            alert(res.msg);
+            this.error = res.msg;
+          }
+        }, error => {
+          console.log(JSON.stringify(error));
+          this.error = error;
+        });
+    }
+
+  }
+
+  partner() {
+    window.open('mailto:info@daofab.com?subject=Partner with DAOFAB&body=Partner with DAOFAB');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
     AppRoutingModule
   ],
   providers: [],
